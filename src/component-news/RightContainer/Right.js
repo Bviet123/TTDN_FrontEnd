ab099bd0acb59a303966fcf8cb41d3f78f79b62c
Bviet123 <buiducvan12346@gmail.com>
2024-06-28 08:23:15 +0700

    UpdateCode

--- src/component-news/RightContainer/Right.js
+++ src/component-news/RightContainer/Right.js
@@ -12,15 +12,15 @@ function Right() {
             <div className="aside-right">
                 <div className='aside-around'>
                     <img className='right-pic' src={Pic} alt="The holder pictrue" />
-                    <b><a href='NewsDetails' className='text_link'><p className='Type'>Hội nghị về phương pháp gải quyết...</p></a></b>
+                    <b><a href='NewsDetails' className='text_link'><p className='Type'>Cuộc thi công nghệ trẻ....</p></a></b>
                 </div>
                 <div className='aside-around'>
                     <img className='right-pic' src={Pic} alt="The holder pictrue" />
-                    <b><a href='NewsDetails' className='text_link'><p className='Type'>Hội nghị về phương pháp gải quyết...</p></a></b>
+                    <b><a href='NewsDetails' className='text_link'><p className='Type'>Cuộc thi công nghệ trẻ....</p></a></b>
                 </div>
                 <div className='aside-around'>
                     <img className='right-pic' src={Pic} alt="The holder pictrue" />
-                    <b><a href='NewsDetails' className='text_link'><p className='Type'>Hội nghị về phương pháp gải quyết...</p></a></b>
+                    <b><a href='NewsDetails' className='text_link'><p className='Type'>Cuộc thi công nghệ trẻ....</p></a></b>
                 </div>
             </div>
             <div>
@@ -29,15 +29,15 @@ function Right() {
             <div className="aside-right">
                 <div className='aside-around'>
                     <img className='right-pic' src={Pic} alt="The holder pictrue" />
-                    <b><a href='NewsDetails' className='text_link'><p className='Type'>Hội nghị về phương pháp gải quyết...</p></a></b>
+                    <b><a href='NewsDetails' className='text_link'><p className='Type'>Cuộc thi công nghệ trẻ....</p></a></b>
                 </div>
                 <div className='aside-around'>
                     <img className='right-pic' src={Pic} alt="The holder pictrue" />
-                    <b><a href='NewsDetails' className='text_link'><p className='Type'>Hội nghị về phương pháp gải quyết...</p></a></b>
+                    <b><a href='NewsDetails' className='text_link'><p className='Type'>Cuộc thi công nghệ trẻ....</p></a></b>
                 </div>
                 <div className='aside-around'>
                     <img className='right-pic' src={Pic} alt="The holder pictrue" />
-                    <b><a href='NewsDetails' className='text_link'><p className='Type'>Hội nghị về phương pháp gải quyết...</p></a></b>
+                    <b><a href='NewsDetails' className='text_link'><p className='Type'>Cuộc thi công nghệ trẻ....</p></a></b>
                 </div>
             </div>
 
--- src/pages/News_Details/NewsDetails.js
+++ src/pages/News_Details/NewsDetails.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import './NewsDetails.css';
 import Nav from '../../component-news/NavigationBar/Nav';
 import Left from '../../component-news/LeftContainer/Left';
@@ -23,14 +22,14 @@ function NewsDetail() {
                             <p>Du lịch</p>
                         </div>
                         <div className='content-title'>
-                            <p>Du khách Hàn Quốc chạy xe máy thăm Hội An bị lạc đường đến miền núi</p>
+                            <p className='NewsDetails_Text'>Du khách Hàn Quốc chạy xe máy thăm Hội An bị lạc đường đến miền núi</p>
                         </div>
                         <div className='content-detail'>
-                            <p>Một nam du khách Hàn Quốc trong lúc chạy xe máy tham quan ở Hội An nhưng lại đi lạc đường đến tận huyện miền núi Bắc Trà My, sau đó được lực lượng công an giúp tìm được người thân, hỗ trợ ăn uống, nghỉ ngơi.</p>
+                            <p className='NewsDetails_Text'>Một nam du khách Hàn Quốc trong lúc chạy xe máy tham quan ở Hội An nhưng lại đi lạc đường đến tận huyện miền núi Bắc Trà My, sau đó được lực lượng công an giúp tìm được người thân, hỗ trợ ăn uống, nghỉ ngơi.</p>
                             <img className='Pic_item' src={itemPic} Alt="The holder Pictrure" />
-                            <p>Chiều 26-6, thông tin từ Công an tỉnh Quảng Nam cho hay lực lượng Công an xã Trà Tân, huyện Bắc Trà My vừa hỗ trợ một nam du khách người Hàn Quốc đoàn tụ cùng người thân gia đình sau thời gian bị lạc đường, lạc người thân trong lúc tham quan ở Hội An.
-
-                                Theo đó qua công tác quản lý địa bàn, khoảng 6h30 cùng ngày, lực lượng công an xã này phát hiện một người nước ngoài có các biểu hiện lạ.</p>
+                            <p className='NewsDetails_Text'>Chiều 26-6, thông tin từ Công an tỉnh Quảng Nam cho hay lực lượng Công an xã Trà Tân, huyện Bắc Trà My vừa hỗ trợ một nam du khách người Hàn Quốc đoàn tụ cùng người thân gia đình sau thời gian bị lạc đường, lạc người thân trong lúc tham quan ở Hội An.
+                            Theo đó qua công tác quản lý địa bàn, khoảng 6h30 cùng ngày, lực lượng công an xã này phát hiện một người nước ngoài có các biểu hiện lạ.</p>
+                            
                         </div>
                       
                     </div>
